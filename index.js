df213b349715298a5a0ffe9a807c5213e5faf157
Enes Buyukcelebi <enessalihbcelebi@fmail.com>
2023-01-27 17:43:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here
+class Breakfast {
+  
+    constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+let breakfast1 = new Breakfast("eggs","juice");
+let breakfast2 = new Breakfast("fries", "milk");
+
+class Lunch {
+    
+      constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  let Lunch1 = new Lunch("Ceasar");
+  let Lunch2 = new Lunch("rice");
+  let Lunch3 = new Lunch("juice");
+
+  class Dinner {
+    #dessert;
+      constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert;
+    }
+  }
+  
+  let Dinner1 = new Dinner("Ceasar");
+  let Dinner2 = new Dinner("rice");
+  let Dinner3 = new Dinner("manti");
+  let Dinner4 = new Dinner("helva"); 
